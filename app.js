b9bc11d5ff23954bbe3f8fc74f7528cb40344f91
katie redmond <katieredmond@dn0a2273e2.sunet>
2014-02-05 20:37:15 -0800

    added handlebar pages for picking class, adding question

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var addQ = require('./routes/addQ');
+var pickClass = require ('./routes/pickClass');
 // Example route
 // var user = require('./routes/user');
 
@@ -36,6 +38,8 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/pickClass', pickClass.view);
+app.get('/addQ', addQ.view);
 // Example route
 // app.get('/users', user.list);
 
